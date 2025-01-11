c61238f23f61795d09fd1ebcda2a43a8cb5d7c85
Luka22-sys <lukacho.metreveli@gmail.com>
2025-01-11 18:29:04 +0400

    sixth

---
+++ level 34/classwork/CW1.py
@@ -0,0 +1,7 @@
+favorite_movies = ("Bladerunner 2049", "Star Wars: A New Hope", "Star Wars: The Empire Strikes Back", 
+                   "Star Wars: Return of the Jedi", "Star Wars: The Force Awakens", "Star Wars: The Last Jedi")
+
+first_movie, *remaining_movies = favorite_movies
+
+print(first_movie)
+print(remaining_movies)
